877a8f3fdea8df9d0b72573e656da677368c905a
Neha-Titirmare <nehatitirmare343@gmail.com>
2026-06-01 14:29:03 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+
+        System.out.print("Enter operator (+, -, *, /): ");
+        char op = sc.next().charAt(0);
+
+        switch(op) {
+            case '+':
+                System.out.println("Result = " + (num1 + num2));
+                break;
+
+            case '-':
+                System.out.println("Result = " + (num1 - num2));
+                break;
+
+            case '*':
+                System.out.println("Result = " + (num1 * num2));
+                break;
+
+            case '/':
+                System.out.println("Result = " + (num1 / num2));
+                break;
+
+            default:
+                System.out.println("Invalid Operator");
+        }
+
+        sc.close();
+    }
+}
